d58be7412e44b116968fa1abba1247bd448c37ec
Bruno Andrade <brunoandrade99@gmail.com>
2021-11-05 01:27:59 -0300

    create routes to navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import React from "react";
 
+import { NavBar, Garage, Wishlist } from "./components";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
 function App() {
   return (
     <div className="App">
-      <h1>Minha Garagem</h1>
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <Garage />
+          </Route>
+          <Route path="/desejos">
+            <Wishlist />
+          </Route>
+        </Switch>
+        <NavBar />
+      </Router>
     </div>
   );
 }
